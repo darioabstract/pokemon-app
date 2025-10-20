eec25072ec3fd371210051585a4556cca0ff8906
Dario Milani <dario.milani@abstract.it>
2025-10-20 11:26:39 +0200

    added main call and response types

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./App.css";
 import mainCall from './api/mainCall';
+import { IGetPokemonData} from './types/dataTypes';
 
 
 export const App = () => {
 
-  useEffect(() => {
-  mainCall.get(``,{}).then((res) => console.log(res))
-})
+const [pokemonList, setPokemonList] = useState<IGetPokemonData[] | []>()
 
+useEffect(() => {
+    mainCall.get('pokemon', {})
+      .then((res) => setPokemonList(res.data.results))
+      .catch((err) => console.error('Failed to fetch Pokémon:', err));
+}, []);
+  
+  console.log(pokemonList)
+  
   return (
     <div className="App">
       <div>Pokemon</div>
+      {pokemonList && pokemonList.map((item, index) => {
+        return (
+
+          <div key={index}>{item && item.name}</div>
+       )
+     })}
+    
     </div>
   );
 }
